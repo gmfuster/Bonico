2bb90c0458f206d8eee0c36efda47021242849c9
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2020-12-02 21:00:41 -0500

    changing counting, in progress

--- src/Views/KidsActivities/Counting.jsx
+++ src/Views/KidsActivities/Counting.jsx
@@ -12,31 +12,42 @@ class Counting extends Component {
         this.digit1 = 0; this.digit2 = 0;      
     }             
     //todo see how to pass the parameter to one function without it being called on load
-    nextDigit1 = () => {
-        (this.digit1 == 9)? this.digit1 = 0 : this.digit1 ++;
-        this.setState( { digit1: this.digit1})
-    }
-    nextDigit2 = () => {
-        (this.digit2 == 9)? this.digit2 = 0 : this.digit2 ++;
-        this.setState( { digit2: this.digit2})
-    }
+    nextDigit1 = (d, s) => {
+
+        switch(d) {
+            case "1":
+                if (s == "UP"){
+                    (this.digit1 == 9)? this.digit1 = 0 : this.digit1 ++;
+                }else{
+                    (this.digit1 == 0)? this.digit1 = 9 : this.digit1 --;
+                }                
+                this.setState( { digit1: this.digit1})
+                break;
+            case "2":
+                if (s == "UP"){
+                    (this.digit2 == 9)? this.digit2 = 0 : this.digit2 ++;
+                }else{
+                    (this.digit2 == 0)? this.digit2 = 9 : this.digit2 --;
+                }                  
+                this.setState( { digit2: this.digit2})
+        }
+        
+    }   
             
     showResult = () => {
         this.setState( {result : this.result});
     }
 
-    render() {
+    render() {      
 
-        var numberStyle = {
-            width:"100%",            
-            textAlign: "center",
-            display:"flex",
-            flexDirection:"row",
-            justifyContent:"center",                     
-            alignItems:"flex-start",
-            fontSize:"20vw",    
-            cursor:"pointer"        
-        }
+       var divHorContainer = {
+        display:"flex",
+        flexDdirection:"row",
+        justifyContent:"center",
+        userSelect:"none"
+    }
+    
+       
       return(
   
       
@@ -45,13 +56,39 @@ class Counting extends Component {
          <div className="TopMarginToDealWithNavBarAll CenterMe">             
            Click on each digit (number) to get the next one.  Pay attention to have both numbers together form the bigger number.  You can set the first number at 2 and Click
            on the second one to see 20, 21, 22 or have a 5 on the second number and click on the first one to see 15, 25, 35 etc.<br/>                       
-                        
-            <div style={numberStyle}>                                                
-                                
-                <span onClick={this.nextDigit1} style={{ color:"deeppink"}} >{this.state.digit1 }</span>                                 
-                <span onClick={this.nextDigit2}>{this.state.digit2}</span>                                                 
-            </div>                  
            
+            <div>                   
+                <div style = {divHorContainer}>
+                    <button style = {{backgroundColor:"navy", width:"20vw" }} onClick={() => this.nextDigit("1", "UP")}>+</button>
+                    <button  style = {{backgroundColor:"deeppink", width:"20vw" }} onClick={() => this.nextDigit("2", "UP")}>-</button> <br/>
+                </div>   
+                <br/>   
+                <div style = {divHorContainer}>
+                    <span style = {{ color:"white", backgroundColor:"navy", width:"20vw", fontSize:"18vw", textAlign:"center"}}>{this.state.digit1 } </span>
+                    <span style = {{ color:"white", backgroundColor:"deeppink", width:"20vw", fontSize:"18vw", textAlign:"center"}}>{this.state.digit2}</span> <br/>
+                </div>                              
+                <br/>   
+                <div style = {divHorContainer}>
+                    <button style = {{backgroundColor:"navy", width:"20vw" }} onClick={() => this.nextDigit("1", "DOWN")}>+</button>  
+                    <button  style = {{backgroundColor:"deeppink", width:"20vw" }} onClick={() => this.nextDigit("2", "DOWN")}>-</button> <br/>
+                </div>                 
+            </div>
+            
+            {/*}
+            <div style={numberStyle}>      
+                <div style={ digitGroupStyle}>
+                    <button style={ buttonStylePink}  onClick={() => this.nextDigit("1", "UP")}></button> 
+                    <span style ={digitStyle}>{this.state.digit1 } </span><br/>
+                    <button onClick={() => this.nextDigit("1", "DOWN")}></button>
+                </div>  
+
+                <div style={ digitGroupStyle}>
+                    <button  onClick={() => this.nextDigit("2", "UP")}></button>  
+                    <span  >{this.state.digit2}</span>     <br/>
+                    <button  onClick={() => this.nextDigit("2", "DOWN")} > </button>                                        
+                </div>
+            </div>                  
+      */}
             
             
         </div>

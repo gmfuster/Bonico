d17841895ad33ef2626aa3ab244d549e168b9cf1
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2020-12-20 19:23:33 -0500

    add filling squares

--- src/Views/KidsActivities/FillSquares.jsx
+++ src/Views/KidsActivities/FillSquares.jsx
@@ -4,25 +4,87 @@ class FillSquares extends Component {
 
     constructor()
     {
-        super();   
-        this.state = {number1:null, number2:null,sq1:null,sq2:null,sq3:null,sq4:null,sq5:null,sq6:null,sq7:null,sq8:null,sq9:null,sq10:null, value:0};
+        super();          
         this.sliderMin = 0;
-        this.sliderMax =10;       
-        this.sliderStep=1; 
-    }                     
-    onValueChanged = (range) => {
+        this.sliderMax =10;     
+        this.currentPink = 0;
+        this.currentBlue = 10;    
+        this.defaultClass = "MathSquareBlue";    
+        this.state = {number1P:this.currentPink, number2B:this.currentBlue, squareClasses: [this.defaultClass, this.defaultClass,this.defaultClass,this.defaultClass,
+            this.defaultClass,this.defaultClass,this.defaultClass,this.defaultClass,this.defaultClass,this.defaultClass]};
+    }      
 
+    getNextColorClass(colorClass){
+
+        switch(colorClass){
+            case "MathSquarePink":
+                return "MathSquareBlue";                
+            case "MathSquareBlue":
+                return "MathSquarePink";
+            case "MathSquareBlank": /*not used now*/
+                return "MathSquarePink";
+        }
+    }
+
+    squareClick = (sqr, index) => {        
+                
+        //sqr is the div
+        let newColorClass = this.getNextColorClass(sqr.className)       ;        
+        let currentClasses = this.state.squareClasses;
+        switch(newColorClass){
+            case "MathSquarePink":
+                this.currentPink++;
+                this.currentBlue--;
+                break;
+            case "MathSquareBlue":
+                this.currentPink--;
+                this.currentBlue++;
+                break;
+        }
+
+        this.setState ({number1P:this.currentPink});
+        this.setState ({number2B:this.currentBlue});
+        currentClasses[index-1] = newColorClass;
+        this.setState ({squareClasses: currentClasses});
+        
+        this.forceUpdate();
     }
 
     render() {                  
+        var containerStyle = {
+            display:"flex",
+            flexDirection: "row",
+            textAlign: "center",
+            flexWrap: "wrap",
+            justifyContent:"center",
+            alignItems: "center"        
+        }
+        
       return(  
           
+     
       <React.Fragment>
          
          <div className="TopMarginToDealWithNavBarAll CenterMe">             
-           Move the slider forward or backward and see how the squares change and how that affects the equation.
-           <div className="sliderContainer">                
-                <input type="range" min="0" max="10" className="slider"/>
+           Click on each square to change its color and see how it affects the equation.
+           <div style={containerStyle}>
+               <span style={{fontSize:"6vw"}}> 
+                    <span style={{color:"deeppink"}}>{this.state.number1P} </span>+ <span style={{color:"skyblue"}}>{this.state.number2B}</span> = 10 
+                </span>
+
+               
+           </div>
+           <div style={containerStyle}>
+                <div className={this.state.squareClasses[0]} onClick={ (e) => this.squareClick(e.currentTarget,1) }></div>
+                <div className={this.state.squareClasses[1]} onClick={ (e) => this.squareClick(e.currentTarget,2)}></div>
+                <div className={this.state.squareClasses[2]} onClick={ (e) => this.squareClick(e.currentTarget,3)}></div>
+                <div className={this.state.squareClasses[3]} onClick={ (e) => this.squareClick(e.currentTarget,4)}></div>
+                <div className={this.state.squareClasses[4]} onClick={ (e) => this.squareClick(e.currentTarget,5)}></div>
+                <div className={this.state.squareClasses[5]} onClick={ (e) => this.squareClick(e.currentTarget,6)}></div>
+                <div className={this.state.squareClasses[6]} onClick={ (e) => this.squareClick(e.currentTarget,7)}></div>
+                <div className={this.state.squareClasses[7]} onClick={ (e) => this.squareClick(e.currentTarget,8)}></div>
+                <div className={this.state.squareClasses[8]} onClick={ (e) => this.squareClick(e.currentTarget,9)}></div>
+                <div className={this.state.squareClasses[9]} onClick={ (e) => this.squareClick(e.currentTarget,10)}></div>
            </div>
         </div>
   

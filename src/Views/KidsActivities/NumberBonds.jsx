f4579fd4adc631f331666e03fcc0913945a6eba0
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2020-12-28 21:10:34 -0500

    minor format changes to number bond

--- src/Views/KidsActivities/NumberBonds.jsx
+++ src/Views/KidsActivities/NumberBonds.jsx
@@ -32,7 +32,7 @@ class NumberBonds extends Component {
         this.signs[this.random1] = ">>>";
         this.signs[this.randomTotal] = ">>>";        
         this.dots[this.random1] = "☻";        
-        this.setState( { total: this.randomTotal, el1: this.random1, el2:"?", signs:this.signs, dots:this.dots});                             
+        this.setState( { total: this.randomTotal, el1: this.random1, el2:"?", signs:this.signs, dots:this.dots, correction:""});                             
         this.forceUpdate();
     }
     resetClick = () => {
@@ -95,55 +95,55 @@ class NumberBonds extends Component {
                 <div style = {divVertContainer}>
 
                     <span style={{backgroundColor:"deeppink", width:"5vw", height:"6vh", borderBottom:"2px solid navy", textAlign:"right", color:"white"}}>
-                        {this.state.signs[9]}9 
+                        <span style = {{ color:"yellow"}}>{this.state.signs[9]}</span> 9 
                         <span style = {{ color:"yellow"}}>{this.state.dots[9]}</span>                    
                     </span>
                     <span style={{backgroundColor:"deeppink", width:"9vw", height:"6vh", borderBottom:"2px solid navy", textAlign:"right", color:"white"}}>
-                        {this.state.signs[8]}8 
+                        <span style = {{ color:"yellow"}}>{this.state.signs[8]}</span> 8 
                         <span style = {{ color:"yellow"}}>{this.state.dots[8]}</span>
                     </span>
                     <span style={{backgroundColor:"deeppink", width:"13vw", height:"6vh", borderBottom:"2px solid navy", textAlign:"right", color:"white"}}>
-                        {this.state.signs[7]}7 
+                        <span style = {{ color:"yellow"}}>{this.state.signs[7]}</span> 7 
                         <span style = {{ color:"yellow"}}>{this.state.dots[7]}</span>
                     </span>
                     <span style={{backgroundColor:"deeppink", width:"17vw", height:"6vh", borderBottom:"2px solid navy", textAlign:"right", color:"white"}}>
-                        {this.state.signs[6]}6 
+                        <span style = {{ color:"yellow"}}>{this.state.signs[6]}</span> 6 
                         <span style = {{ color:"yellow"}}>{this.state.dots[6]}</span>
                     </span>
                     <span style={{backgroundColor:"deeppink", width:"21vw", height:"6vh", borderBottom:"2px solid navy", textAlign:"right", color:"white"}}>
-                        {this.state.signs[5]}5 
+                        <span style = {{ color:"yellow"}}>{this.state.signs[5]}</span> 5 
                         <span style = {{ color:"yellow"}}>{this.state.dots[5]}</span>
                     </span>
                     <span style={{backgroundColor:"deeppink", width:"25vw", height:"6vh", borderBottom:"2px solid navy", textAlign:"right", color:"white"}}>
-                        {this.state.signs[4]}4 
+                    <span style = {{ color:"yellow"}}>{this.state.signs[4]}</span> 4 
                         <span style = {{ color:"yellow"}}>{this.state.dots[4]}</span>
                     </span>
                     <span style={{backgroundColor:"deeppink", width:"29vw", height:"6vh",borderBottom:"2px solid navy", textAlign:"right", color:"white"}}>
-                        {this.state.signs[3]}3 
+                        <span style = {{ color:"yellow"}}>{this.state.signs[3]}</span> 3 
                         <span style = {{ color:"yellow"}}>{this.state.dots[3]}</span>
                     </span>
                     <span style={{backgroundColor:"deeppink", width:"33vw", height:"6vh", borderBottom:"2px solid navy", textAlign:"right", color:"white",}}>
-                        {this.state.signs[2]}2 
+                         <span style = {{ color:"yellow"}}>{this.state.signs[2]}</span> 2 
                         <span style = {{ color:"yellow"}}>{this.state.dots[2]}</span>
                     </span>
                     <span style={{backgroundColor:"deeppink", width:"37vw", height:"6vh", borderBottom:"2px solid navy", textAlign:"right", color:"white"}}>
-                        {this.state.signs[1]}1 
+                        <span style = {{ color:"yellow"}}>{this.state.signs[1]}</span> 1 
                         <span style = {{ color:"yellow"}}>{this.state.dots[1]}</span>
                     </span>
                     <span style={{backgroundColor:"deeppink", width:"41vw", height:"6vh",borderBottom:"2px solid navy", textAlign:"right", color:"white"}}>
-                        {this.state.signs[0]}0 
+                        <span style = {{ color:"yellow"}}>{this.state.signs[0]}</span> 0 
                         <span style = {{ color:"yellow"}}>{this.state.dots[0]}</span>
                     </span>
                 </div>  
 
                 {/*bond */}
                 <div style = {divVertContainer} >
-                    <div style = {{border:"3px solid deeppink", width:"10vw", height:"6vh"}}>{this.state.total}</div>
+                    <div style = {{border:"3px solid deeppink", width:"11vw", height:"7vh"}}>{this.state.total}</div>
                     <div style = {divHorContainer}>
-                        <div style = {{borderRight:"3px solid deeppink", width:"2.5vw", height:"10vh"}}/>
-                        <div style = {{width:"2.5vw", height:"10vh"}}/>
-                        <div style = {{width:"2.5vw", height:"10vh"}}/>
-                        <div style = {{borderLeft:"3px solid deeppink", width:"2.5vw", height:"10vh"}}/>
+                        <div style = {{borderRight:"3px solid deeppink", width:"2.5vw", height:"11vh"}}/>
+                        <div style = {{width:"2.5vw", height:"11vh"}}/>
+                        <div style = {{width:"2.5vw", height:"11vh"}}/>
+                        <div style = {{borderLeft:"3px solid deeppink", width:"2.5vw", height:"11vh"}}/>
                     </div>
                     <div  style = {divHorContainer}>
                         <div style = {{border:"3px solid deeppink", width:"5vw", height:"5vw", borderRadius:"50px"}}>{this.state.el1}</div>
@@ -152,16 +152,16 @@ class NumberBonds extends Component {
                 </div>
 
                 {/*Equation*/}
-                <div style = {{paddingTop:"10vh", paddingLeft:"2vw", fontSize:"5vw"}}>
+                <div style = {{paddingTop:"10vh", paddingLeft:"1vw"}}>
                     <div style = {divVertContainer} > 
                         <div style = {divHorContainer}>
-                            <label style = {{ }}>{this.state.el1}</label>
-                            <span>+</span>
-                            <label>{this.state.el2}</label>
-                            <label>=</label>
-                            <label>{this.state.total}</label>
+                            <label style = {{fontSize:"6vw"}}>{this.state.el1}</label>
+                            <span style = {{fontSize:"6vw"}}>+</span>
+                            <label style = {{fontSize:"6vw"}}>{this.state.el2}</label>
+                            <label style = {{fontSize:"6vw"}}>=</label>
+                            <label style = {{fontSize:"6vw"}}>{this.state.total}</label>
                         </div>
-                        <label style = {{fontSize:"4vw", border:"2px solid salmon" }}>{this.state.correction}</label>
+                        <label style = {{fontSize:"6vw", border:"2px solid salmon" }}>{this.state.correction}</label>
                     </div>
                 </div>
                               

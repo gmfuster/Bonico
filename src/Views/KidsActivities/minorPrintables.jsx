c171d76ef18ec94ca7f09ef0126d872902f5958b
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2021-01-26 20:07:17 -0500

    change the printables format

--- src/Views/KidsActivities/minorPrintables.jsx
+++ src/Views/KidsActivities/minorPrintables.jsx
@@ -7,38 +7,61 @@ function MinorPrintables () {
       return(        
         
       <React.Fragment>
-         <div className="TopMarginToDealWithNavBarAll">
-
-         <br/>
-
-            <div className="myContentsAreGrid1">
-                                
+         <div className="TopMarginToDealWithNavBarAll">            
+            <div class="grid-container3">
+                <div class="grid-item">
+                    <div style={{margin:"3vh"}}>
+                        <a className="buttonPrintable buttonPrintableLang" href="authorvsillustrator.pdf" target="_new">Author/Illustrator!</a>
+                    </div>      
+                    Graphic to indicate what author and illustrator do in a book.
+                </div>
+            <div class="grid-item">
                 <div style={{margin:"3vh"}}>
-                    <a className="buttonPrintable buttonPrintableLang" href="authorvsillustrator.pdf" target="_new">Author vs Illustrator!</a>
-                </div>                                              
-                
-                <div style={{margin:"3vh"}}>
-                    <a className="buttonPrintable buttonPrintableLang" style={{textDecoration:"none"}} href="rhymes.pdf" target="_new">Rhyming Cards!</a>
+                    <a className="buttonPrintable buttonPrintableLang" style={{textDecoration:"none"}} href="rhymes.pdf" target="_new">Rhyming!</a>
                 </div>  
-
+                Print these cards and practice finding which ones rhyme.
+            </div>
+            <div class="grid-item">
                 <div style={{margin:"3vh"}}>
-                <a className="buttonPrintable buttonPrintableLang" style={{textDecoration:"none"}} href="startandendsounds.pdf" target="_new">Being/End Sounds!</a>
+                    <a className="buttonPrintable buttonPrintableLang" style={{textDecoration:"none"}} href="startandendsounds.pdf" target="_new">Being/End Sounds!</a>                    
                 </div> 
+                Cards with letters and some words to use those letters on.  The end sound of one word will be the start sound of another word.
+            </div>  
 
+
+            <div class="grid-item">
                 <div style={{margin:"3vh"}}>
-                <a className="buttonPrintable buttonPrintableMath" style={{textDecoration:"none"}} href="CombiningNumbersAddition.pdf" target="_new">2 and 3 make 5</a>
+                    <a className="buttonPrintable buttonPrintableMath" style={{textDecoration:"none"}} href="CombiningNumbersAddition.pdf" target="_new">Add!</a>
                 </div> 
-
+                Color the squares and write the addition equation to go with them.
+            </div>
+            <div class="grid-item">
                 <div style={{margin:"3vh"}}>
                     <a className="buttonPrintable buttonPrintableMath"  href="patterns.pdf" target="_new">Patterns!</a>
-                </div>  
-
+                </div> 
+                Work on some patterns.
+            </div>
+            <div class="grid-item">
                 <div style={{margin:"3vh"}}>
                     <a className="buttonPrintable buttonPrintableMath"  href="equationproblems.pdf" target="_new">Add/Subtract!</a>
                 </div> 
+                Print these cards with numbers and word problems and identify whether we are adding or subtracting, and build the equation.
+            </div>              
+        </div>       
+            
+                                
+                                                        
+                
+                
 
-            </div>        
-        </div>
+               
+
+                
+                 
+
+               
+
+            </div>                
       </React.Fragment>)
   };  
   

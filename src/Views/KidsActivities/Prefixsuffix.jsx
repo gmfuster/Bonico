951f621c0cd5ed92951e127cc4f1941702c3588c
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2021-02-22 12:10:14 -0500

    Add code to prefix suffix

--- src/Views/KidsActivities/Prefixsuffix.jsx
+++ src/Views/KidsActivities/Prefixsuffix.jsx
@@ -4,7 +4,8 @@ class Prefixsuffix extends Component {
     
     constructor(){
         super();            
-        this.rootWords = ["build", "view","friendly", "fear", "color", "welcome", "try", "like", "school", "do", "buy", "order", "odor"];
+        //should be hardcoded with all lowercase but code is checking anwaway
+        this.rootWords = ["build", "view","friendly", "fear", "color", "welcome", "try", "like", "paint","school", "do", "buy", "order", "odor", "play", "walk"];
         this.state = {currentWord:this.rootWords[0].toUpperCase(), prefix:"", suffix:""};                
     }        
 
@@ -12,35 +13,40 @@ class Prefixsuffix extends Component {
         window.scrollTo(0, 0)
     }
     next = () => {
-        let i = this.rootWords.findIndex(this.state.currentWord);
-        if (i < this.rootWords.length - 1 ){
-            this.setState({currentWord:this.rootWords[i+1].toUpperCase()})
-        } else{
-            this.setState({currentWord:this.rootWords[0].toUpperCase()})
-        }
+        
+        let word = this.state.currentWord.toLowerCase();
+        let i = this.rootWords.findIndex( item => item.toLowerCase() === word.toLowerCase());        
+
+        
+        this.setState({currentWord:this.rootWords[i+1].toUpperCase(), prefix:"", suffix:""})
+        
     }        
 
     prefixsuffix = (text, ps) => {
         let prefix = "";
         let suffix = "";
-        (ps.toUpperCase() === "P")?prefix = text : suffix=text;            
+        (ps.toUpperCase() === "P")? prefix = text : suffix=text;            
         this.setState({suffix:suffix, prefix:prefix});        
     }
     render() {
                                          
-        var buttonStyles = {
+        var buttonStylesPre = {
             width:"8vw",
-            heigth:"6vh",
-            fontSize:"5vh"
+            heigth:"8vh",
+            fontSize:"3vw" ,
+            background:"lightgreen"           
         }
-        var wordStyles = {
-            width:"12vw",
-            heigth:"10vh",
-            fontSize:"8vh",            
-            padding:"3px",            
-            background:"lightblue"
+        var buttonStylesSuf = Object.assign({}, buttonStylesPre, {background:"yellow"});          
+
+        var wordStylesPre = {                       
+            fontSize:"12vh",                                 
+            background:"lightblue",
+            borderTop:"5px solid navy",
+            borderBottom:"5px solid navy",
+            background:"lightgreen"
         }
-        var wordStylesClick = Object.assign({}, wordStyles, {cursor:"pointer", background:"lightpink"});            
+        var wordStylesClick = Object.assign({}, wordStylesPre, {cursor:"pointer", background:"lightblue"});    
+        var wordStylesSuf = Object.assign({}, wordStylesPre, {background:"yellow"});    
 
       return(          
         
@@ -49,19 +55,20 @@ class Prefixsuffix extends Component {
             <div className="CenterMe">
                 Click on each prefix or sufix to see how they change the root word.  Click on the root to get a new word.              
                 <div className="myContentsAreFlex">
-                    <button className="buttonOptions" style={buttonStyles} onClick= { () => this.prefixsuffix("RE", "P")}>RE</button>
-                    <button className="buttonOptions" style={buttonStyles} onClick= { () => this.prefixsuffix("PRE", "P")}>PRE</button>
-                    <button className="buttonOptions" style={buttonStyles} onClick= {() => this.prefixsuffix("DIS", "P")}>DIS</button>
-                    <button className="buttonOptions" style={buttonStyles} onClick= {() => this.prefixsuffix("UN", "P")}>UN</button>
+                    <button className="buttonOptions" style={buttonStylesPre} onClick= { () => this.prefixsuffix("RE", "P")}>RE</button>
+                    <button className="buttonOptions" style={buttonStylesPre} onClick= { () => this.prefixsuffix("PRE", "P")}>PRE</button>
+                    <button className="buttonOptions" style={buttonStylesPre} onClick= {() => this.prefixsuffix("DIS", "P")}>DIS</button>
+                    <button className="buttonOptions" style={buttonStylesPre} onClick= {() => this.prefixsuffix("UN", "P")}>UN</button>
 
-                    <button className="buttonOptions" style={buttonStyles} onClick= {() => this.prefixsuffix("FUL", "S")}>FUL</button>
-                    <button className="buttonOptions" style={buttonStyles} onClick= {() => this.prefixsuffix("LESS", "S")}>LESS</button>
+                    <button className="buttonOptions" style={buttonStylesSuf} onClick= {() => this.prefixsuffix("FUL", "S")}>FUL</button>
+                    <button className="buttonOptions" style={buttonStylesSuf} onClick= {() => this.prefixsuffix("LESS", "S")}>LESS</button>
+                    <button className="buttonOptions" style={buttonStylesSuf} onClick= {() => this.prefixsuffix("ED", "S")}>ED</button>
                 </div>
                 
             <div className="CenterMe">
-                <span style={wordStyles}>{this.state.prefix}</span>
+                <span style={wordStylesPre}>{this.state.prefix}</span>
                 <span style={wordStylesClick} onClick={this.next}>{this.state.currentWord}</span>
-                <span style={wordStyles}>{this.state.suffix}</span>
+                <span style={wordStylesSuf}>{this.state.suffix}</span>
                     
             </div>   
                

ed9fadaa4fa89dda08d927c45a4658deda8f8403
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2021-04-03 19:29:51 -0400

    change description

--- src/Components/DescriptionSections.jsx
+++ src/Components/DescriptionSections.jsx
@@ -4,13 +4,13 @@ function DescriptionSections () {
     
         return(
             <React.Fragment>
-                <div className="DescriptionSectionText">                                      
-                   Not designed for phone size.<br/>                                                
+                <div className="DescriptionSectionText">                                                         
+                   <img style = {{ height:"8%", width:"8%"}} src={process.env.REACT_APP_FOR_PATH_FOR_PICS + '/images/requirestouch.svg'}  alt="Requires touch screen"  />    
+                   Look for these on some of the activities
+                   <img style = {{ height:"8%", width:"8%"}} src={process.env.REACT_APP_FOR_PATH_FOR_PICS + '/images/requiresgrownup.svg'}  alt="Requires grown up"  />    
                 </div>
 
-                <div style={{background:"lightyellow", margin:"15px", padding:"15px"}}>
-                    Contact me on <a href="https://gnmfuster.medium.com/" target="_new">Medium</a> or <a href="https://twitter.com/gnmfuster">Twitter</a>.  
-                    <br/>Check out my book on Amazon:
+                <div style={{background:"lightyellow", margin:"15px", padding:"15px"}}>                    
                     <div className="CenterMe" >
                         <iframe type="text/html" className="cardDiv" frameBorder="0"   title="jonandhismomtalkabout"                    
                             src="https://read.amazon.com/kp/card?asin=B08JH9GXJ9&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_tKpFFbQZZHSP4" ></iframe>     

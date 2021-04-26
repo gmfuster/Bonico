f4966c0bd91389d3cbe71285c090dfc50c671fc9
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2021-04-26 19:20:59 -0400

    Change the main image, need to add animation

--- src/Components/MainImage.jsx
+++ src/Components/MainImage.jsx
@@ -3,18 +3,31 @@ import React from "react";
 function MainImage () {       
    
       const pathPicSrc = process.env.REACT_APP_FOR_PATH_FOR_PICS +  "/images/boyplaybeach.png";
+      const learningPic = process.env.REACT_APP_FOR_PATH_FOR_PICS +  "/images/learning.svg";
+      const isPic = process.env.REACT_APP_FOR_PATH_FOR_PICS +  "/images/is.svg";
+      const funPic = process.env.REACT_APP_FOR_PATH_FOR_PICS +  "/images/fun.svg";
 
       var imageStyle = {        
-        height: "55vh",
-        width:"100%"
+        height: "100%",
+        width:"100%",    
+        opacity:"85%"    
       }      
+
+      let divStyle ={
+        position:"relative",
+        width:"100%",
+        height: "55vh",
+      }
 
      return(
         
         <React.Fragment>    
-            <div className="imageContainer TopMarginToDealWithNavBarImg">
+            <div className="imageContainer TopMarginToDealWithNavBarImg" style={divStyle}>
               <img style = {imageStyle} src = {pathPicSrc} alt="boy at beach"/>  
-              <div className="bottomImageText">Learning is Fun!</div>
+              {/*<div className="bottomImageText">Learning is Fun!</div>*/}
+              <img style = {{position:"absolute", width:"45vw", height:"35vh", top:"10%", left:"0%"}} src = {learningPic} alt="LEARNING"/>  
+              <img style = {{position:"absolute", width:"35vw", height:"25vh", top:"45%", left:"25%"}} src = {isPic} alt="IS"/>  
+              <img style = {{position:"absolute", width:"30vw", height:"20vh", top:"35%", right:"5%"}} src = {funPic} alt="FUN"/>  
             </div>        
         </React.Fragment>
       )
